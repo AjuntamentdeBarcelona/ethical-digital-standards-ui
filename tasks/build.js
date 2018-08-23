981cad8ef02f75708404dc2f15a652e132226f12
Marta Armada <marta@swwweet.com>
2018-08-23 10:58:15 +0200

    Precss plugin

--- tasks/build.js
+++ tasks/build.js
@@ -1,52 +1,54 @@
-'use strict'
+'use strict';
 
-const autoprefixer = require('autoprefixer')
-const browserify = require('browserify')
-const buffer = require('vinyl-buffer')
-const concat = require('gulp-concat')
-const cssnano = require('cssnano')
-const fs = require('fs')
-const imagemin = require('gulp-imagemin')
-const map = require('map-stream')
-const merge = require('merge-stream')
-const mkdirp = require('mkdirp')
-const path = require('path')
-const postcss = require('gulp-postcss')
-const postcssCalc = require('postcss-calc')
-const postcssImport = require('postcss-import')
-const postcssUrl = require('postcss-url')
-const postcssVar = require('postcss-custom-properties')
-const uglify = require('gulp-uglify')
-const vfs = require('vinyl-fs')
+const autoprefixer = require('autoprefixer');
+const browserify = require('browserify');
+const buffer = require('vinyl-buffer');
+const concat = require('gulp-concat');
+const cssnano = require('cssnano');
+const fs = require('fs');
+const imagemin = require('gulp-imagemin');
+const map = require('map-stream');
+const merge = require('merge-stream');
+const mkdirp = require('mkdirp');
+const path = require('path');
+const postcss = require('gulp-postcss');
+const postcssCalc = require('postcss-calc');
+const postcssImport = require('postcss-import');
+const postcssUrl = require('postcss-url');
+const postcssVar = require('postcss-custom-properties');
+//const precss = require('precss');
+const uglify = require('gulp-uglify');
+const vfs = require('vinyl-fs');
 
 module.exports = (src, dest) => {
-  const opts = { base: src, cwd: src }
+  const opts = { base: src, cwd: src };
   const postcssPlugins = [
     postcssImport(),
     postcssUrl([
       {
         filter: '**/~typeface-*/files/*',
-        url: (asset) => {
-          const relpath = asset.pathname.substr(1)
-          const abspath = path.resolve('node_modules', relpath)
-          const basename = path.basename(abspath)
-          const destpath = path.join(dest, 'font', basename)
+        url: asset => {
+          const relpath = asset.pathname.substr(1);
+          const abspath = path.resolve('node_modules', relpath);
+          const basename = path.basename(abspath);
+          const destpath = path.join(dest, 'font', basename);
           if (!fs.existsSync(destpath)) {
-            const dirname = path.dirname(destpath)
+            const dirname = path.dirname(destpath);
             if (!fs.existsSync(dirname)) {
-              mkdirp.sync(dirname)
+              mkdirp.sync(dirname);
             }
-            fs.copyFileSync(abspath, destpath)
+            fs.copyFileSync(abspath, destpath);
           }
-          return path.join('..', 'font', basename)
-        },
-      },
+          return path.join('..', 'font', basename);
+        }
+      }
     ]),
     postcssVar(),
     postcssCalc(),
+    //precss(),
     autoprefixer({ browsers: ['last 2 versions'] }),
-    cssnano({ preset: 'default' }),
-  ]
+    cssnano({ preset: 'default' })
+  ];
 
   return merge([
     vfs
@@ -59,8 +61,11 @@ module.exports = (src, dest) => {
       .pipe(
         // see https://gulpjs.org/recipes/browserify-multiple-destination.html
         map((file, next) => {
-          file.contents = browserify(file.relative, { basedir: src, detectGlobals: false }).bundle()
-          next(null, file)
+          file.contents = browserify(file.relative, {
+            basedir: src,
+            detectGlobals: false
+          }).bundle();
+          next(null, file);
         })
       )
       .pipe(buffer())
@@ -76,6 +81,6 @@ module.exports = (src, dest) => {
 
     vfs.src('layouts/*.hbs', opts),
 
-    vfs.src('partials/*.hbs', opts),
-  ]).pipe(vfs.dest(dest))
-}
+    vfs.src('partials/*.hbs', opts)
+  ]).pipe(vfs.dest(dest));
+};
