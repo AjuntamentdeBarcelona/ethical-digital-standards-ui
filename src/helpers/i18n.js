5a370a3538343f1a48ec6595ae9d465b9adea6e2
htmlboy <javier@swwweet.com>
2018-11-07 16:41:06 +0100

    Download button fixes

--- src/helpers/i18n.js
+++ src/helpers/i18n.js
@@ -16,7 +16,7 @@ const uiLocales = {
     landingIntroP2:
       "A decided change that puts the general public at the centre of designing “digital by default” government services focused on technological sovereignty, free software and open code, and the ethical use of data, in order to be more open and transparent. Using agile methodologies and open technologies in order to be more effective. And transforming public contracting in order to promote innovation in the local talent industry, strengthening the country's small and medium-sized companies. A change that eventually leads to a more open, simple and enriching relationship with the general public as a whole.",
     landingPdfUrl: 'http://ajuntament.barcelona.cat/digital/sites/default/files/LE_MesuradeGovern_EN_9en.pdf',
-    landingCtaText: 'government measure for open digitisation',
+    landingCtaText: 'Government measure for open digitisation (PDF)',
     landingCtaAction: 'Download',
     navigationMenuDownloadPdf: 'Download PDF',
     navigationMenuGlossary: 'Guides glossary',
@@ -40,7 +40,7 @@ const uiLocales = {
     landingIntroP2:
       'Un cambio decidido que pone a la ciudadanía en el centro para diseñar un gobierno de servicios “digitales por defecto” enfocado a la soberanía tecnológica, al software libre y de código abierto, y al uso ético de los datos para ser más abierto y transparente, que utiliza metodologías ágiles y tecnologías abiertas para ser más efectivo y que transforma la contratación pública para impulsar la innovación del conjunto de la industria local de talento, potenciando las pequeñas y medianas empresas del país. Un cambio que dé lugar, finalmente, a una relación más abierta, sencilla y enriquecedora con el conjunto de la ciudadanía.',
     landingPdfUrl: 'http://ajuntament.barcelona.cat/digital/sites/default/files/LE_MesuradeGovern_ESP_9en.pdf',
-    landingCtaText: 'medida del gobierno para la digitalización abierta',
+    landingCtaText: 'Medida del gobierno para la digitalización abierta (PDF)',
     landingCtaAction: 'Descargar',
     navigationMenuDownloadPdf: 'Descargar PDF',
     navigationMenuGlossary: 'Glosario de las guías',
@@ -64,7 +64,7 @@ const uiLocales = {
     landingIntroP2:
       'Un canvi decidit que posa a la ciutadania al centre per dissenyar un govern de serveis “digitals per defecte” enfocat a la sobirania tecnològica, al programari lliure i de codi obert, i a l’ús ètic de les dades per ser més obert i transparent. Que utilitza metodologies àgils i tecnologies obertes per ser més efectiu. I que transforma la contractació pública per impulsar la innovació del conjunt de la industria local de talent, potenciant les petites i mitjanes empreses del país. Un canvi que doni lloc, finalment, a una relació més oberta, senzilla i enriquidora amb el conjunt de la ciutadania.',
     landingPdfUrl: 'http://ajuntament.barcelona.cat/digital/sites/default/files/le_mesuradegovern_v2.pdf',
-    landingCtaText: 'mesura del govern per a la digitalització oberta',
+    landingCtaText: 'Mesura del govern per a la digitalització oberta (PDF)',
     landingCtaAction: 'Descarregar',
     navigationMenuDownloadPdf: 'Descarregar PDF',
     navigationMenuGlossary: 'Glossari de les guies',
